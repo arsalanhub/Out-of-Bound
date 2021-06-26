72d12fa9e880ea8a72684c5e9ae8242bd0ab7c7a
Tech Youths <arsalanmohd237@gmail.com>
2021-06-26 17:06:53 +0530

    clients section

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // Owl-carousel
+
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            560: {
+                items: 2
+            }
+        }
+    })
+});
