db6fbc318810334e27c5f632df4569eb88f02353
arsalanhub <arsalanmohd237@gmail.com>
2021-10-15 17:25:18 +0530

    client updates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Category from './components/Category';
 import About from './components/About';
+import Client from './components/Client';
 
 function App() {
   return (
      <>
        <Navbar />
        <Category />
+       <Client />
        <About />
      </>  
   );
---
+++ src/components/Client.js
@@ -0,0 +1,88 @@
+import React from 'react'
+
+export default function Client() {
+    return (
+        <main class="site-main">
+        <section className="about-area">
+            <div className="container">
+                <div className="row text-center">
+                    <div className="col-12">
+                        <div className="about-title">
+                            <h1 id="About-Us" className="text-uppercase title-h1">Client Say about us</h1>
+                            <p className="para">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus, deleniti
+                                recusandae. Esse incidunt rem repellendus ab voluptates maxime? Nemo, numquam!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="container carousel py-lg-5">
+                <div className="owl-carousel owl-theme">
+                    <div className="client row">
+                        <div className="col-lg-4 col-md-12 client-img">
+                            <img src="./img/testimonials/t1.jpg" alt="img1" className="img-fluid" />
+                        </div>
+                        <div className="col-lg-8 col-md-12 about-client">
+                            <h4 className="text-uppercase">John Martin</h4>
+                            <p className="para">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem architecto
+                                consequuntur ratione, obcaecati corrupti deserunt.</p>
+                        </div>
+                    </div>
+                    <div className="client row">
+                        <div className="col-lg-4 col-md-12 client-img">
+                            <img src="./img/testimonials/t2.jpg" alt="img2" className="img-fluid" />
+                        </div>
+                        <div className="col-lg-8 col-md-12 about-client">
+                            <h4 className="text-uppercase">Mac Hill</h4>
+                            <p className="para">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem architecto
+                                consequuntur ratione, obcaecati corrupti deserunt.</p>
+                        </div>
+                    </div>
+                    <div className="client row">
+                        <div className="col-lg-4 col-md-12 client-img">
+                            <img src="./img/testimonials/t1.jpg" alt="img1" className="img-fluid" />
+                        </div>
+                        <div className="col-lg-8 col-md-12 about-client">
+                            <h4 className="text-uppercase">John Martin</h4>
+                            <p className="para">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem architecto
+                                consequuntur ratione, obcaecati corrupti deserunt.</p>
+                        </div>
+                    </div>
+                    <div className="client row">
+                        <div className="col-lg-4 col-md-12 client-img">
+                            <img src="./img/testimonials/t2.jpg" alt="img2" className="img-fluid" />
+                        </div>
+                        <div className="col-lg-8 col-md-12 about-client">
+                            <h4 className="text-uppercase">Mac Hill</h4>
+                            <p className="para">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem architecto
+                                consequuntur ratione, obcaecati corrupti deserunt.</p>
+                        </div>
+                    </div>
+                    <div className="client row">
+                        <div className="col-lg-4 col-md-12 client-img">
+                            <img src="./img/testimonials/t1.jpg" alt="img1" className="img-fluid" />
+                        </div>
+                        <div className="col-lg-8 col-md-12 about-client">
+                            <h4 className="text-uppercase">John Martin</h4>
+                            <p className="para">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem architecto
+                                consequuntur ratione, obcaecati corrupti deserunt.</p>
+                        </div>
+                    </div>
+                    <div className="client row">
+                        <div className="col-lg-4 col-md-12 client-img">
+                            <img src="./img/testimonials/t2.jpg" alt="img2" className="img-fluid" />
+                        </div>
+                        <div className="col-lg-8 col-md-12 about-client">
+                            <h4 className="text-uppercase">Mac Hill</h4>
+                            <p className="para">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem architecto
+                                consequuntur ratione, obcaecati corrupti deserunt.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </main>
+    )
+}
