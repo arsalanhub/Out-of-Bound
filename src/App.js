d9c5bd4e6961038a3a59a048ea7c5c8627648d76
arsalanhub <arsalanmohd237@gmail.com>
2021-10-15 15:59:47 +0530

    about / footer added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Category from './components/Category';
+import About from './components/About';
 
 function App() {
   return (
      <>
        <Navbar />
        <Category />
+       <About />
      </>  
   );
 }
